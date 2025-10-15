caffa431545a91358e56b7674fe397975ab96131
Ic3berg0522 <michaelb1643@gmail.com>
2025-10-15 14:02:16 -0500

    Add files via upload

--- ALUUnit.vhd
+++ ALUUnit.vhd
@@ -57,7 +57,7 @@ begin
 
 
   
- -- Shift mode select (00=SLL, 01=SRL, 10=SRA/SRAI)
+ -- Shift mode select (00=SLL, 01=SRL, 10=SRA 11=SRAI)
   with ALU_op select
     sh_mode <= "00" when "0111",  -- SLL
                 "01" when "1000",  -- SRL
